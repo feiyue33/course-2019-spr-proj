26abd82bac5d8f6e6da86b5c42025524295d87ff
Tian Gao <gaotian@bu.edu>
2019-04-25 20:03:58 -0400

    add features

--- emmaliu_gaotian_xli33_yuyangl/visualization.py
+++ emmaliu_gaotian_xli33_yuyangl/visualization.py
@@ -9,6 +9,10 @@ import re
 import time
 from google.cloud import translate
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import tempfile
+import subprocess
 
 # Google Cloud
 # To get the credential:
@@ -20,6 +24,76 @@ credential_path = "../auth.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 translate_client = translate.Client()
 analyzer = SentimentIntensityAnalyzer()
+
+PORT = 5002
+HOST = '127.0.0.1'
+SERVER_ADDRESS = '{host}:{port}'.format(host=HOST, port=PORT)
+FULL_SERVER_ADDRESS = 'http://' + SERVER_ADDRESS
+
+
+def TemproraryHttpServer(page_content_type, raw_data):
+    """
+    A simpe, temprorary http web server on the pure Python 3.
+    It has features for processing pages with a XML or HTML content.
+    """
+
+    class HTTPServerRequestHandler(BaseHTTPRequestHandler):
+        """
+        An handler of request for the server, hosting XML-pages.
+        """
+
+        def do_GET(self):
+            """Handle GET requests"""
+
+            # response from page
+            self.send_response(200)
+
+            # set up headers for pages
+            content_type = 'text/{0}'.format(page_content_type)
+            self.send_header('Content-type', content_type)
+            self.end_headers()
+
+            # writing data on a page
+            self.wfile.write(bytes(raw_data, encoding='utf'))
+
+            return
+
+    if page_content_type not in ['html', 'xml']:
+        raise ValueError('This server can serve only HTML or XML pages.')
+
+    page_content_type = page_content_type
+
+    # kill a process, hosted on a localhost:PORT
+    subprocess.call(['fuser', '-k', '{0}/tcp'.format(PORT)])
+
+    # Started creating a temprorary http server.
+    httpd = HTTPServer((HOST, PORT), HTTPServerRequestHandler)
+
+    # run a temprorary http server
+    httpd.serve_forever()
+
+
+def run_html_server(html_data=None):
+
+    if html_data is None:
+        html_data = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>Page Title</title>
+        </head>
+        <body>
+        <h1>This is a Heading</h1>
+        <p>This is a paragraph.</p>
+        </body>
+        </html>
+        """
+
+    # open in a browser URL and see a result
+    webbrowser.open(FULL_SERVER_ADDRESS)
+
+    # run server
+    TemproraryHttpServer('html', html_data)
 
 
 def remove_emoji(string):
@@ -102,6 +176,13 @@ class visualization(dml.Algorithm):
         folium.LayerControl().add_to(sentiment_map)
         sentiment_map.save('sentiment_map.html')
 
+        tmp = tempfile.NamedTemporaryFile()
+        sentiment_map.save(tmp.name)
+        with open('sentiment_map.html') as f:
+            folium_map_html = f.read()
+
+        run_html_server(folium_map_html)
+
         repo.logout()
 
         endTime = datetime.datetime.now()
@@ -143,7 +224,7 @@ class visualization(dml.Algorithm):
         return doc
 
 
-visualization.execute()
+# visualization.execute()
 # doc = visualization.provenance()
 # print(doc.get_provn())
 # print(json.dumps(json.loads(doc.serialize()), indent=4))
